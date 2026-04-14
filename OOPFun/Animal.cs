c73c433480ca93c69d85ec23f78327f41766b95f
QACS-TL <PBehague@qa.com>
2026-04-14 10:04:24 +0100

    Add project files.

---
+++ OOPFun/Animal.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOPFun
+{
+    public class Animal
+    {
+        public string name = "Anonymous";
+        public int limbCount = 4;
+        public string colour = "Brown";
+        public int health = 100;
+
+        public string Eat(string food)
+        {
+            return $"I'm a {colour} animal called {name} using some of my {limbCount} limbs to eat {food}";
+        }
+
+        public string Move(int distance, string direction="up")
+        {
+            return $"I'm a {colour} animal called {name} using some of my {limbCount} limbs to move {direction} for {distance} metres";
+        }
+    }
+}
---
+++ OOPFun/Program.cs
@@ -0,0 +1,20 @@
+﻿// See https://aka.ms/new-console-template for more information
+using OOPFun;
+
+Animal ani1 = new Animal();
+Animal ani2 = new Animal();
+
+ani1.name = "Janet";
+ani1.colour = "Purple";
+ani1.limbCount = 6;
+
+ani2.name = "Cindy";
+ani2.colour = "Green";
+ani2.limbCount = 8;
+ani2.health = 80;
+
+Console.WriteLine(ani1.Move(10, "down"));
+Console.WriteLine(ani2.Move(15));
+
+Console.WriteLine(ani1.Eat("biscuits"));
+Console.WriteLine(ani2.Eat("cake"));
