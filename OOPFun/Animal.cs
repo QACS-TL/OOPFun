130a6d035d21a6dda3812bf8c660147dcf2f50cb
QACS-TL <PBehague@qa.com>
2026-04-14 11:19:40 +0100

    fields changed to properties

--- OOPFun/Animal.cs
+++ OOPFun/Animal.cs
@@ -8,19 +8,59 @@ namespace OOPFun
 {
     public class Animal
     {
-        public string name = "Anonymous";
-        public int limbCount = 4;
-        public string colour = "Brown";
-        public int health = 100;
+        public string Name { get; set; } = "Anonymous";
+        //private int limbCount = 4;
+        public string Colour { get; set; } = "Brown";
+        public int Health { get; set; } = 100;
+
+        // The Java Solution
+        //public int GetLimbCount(){
+        //     return limbCount;
+        //}
+
+        //public void SetLimbCount(int value)
+        //{
+        //    if (value < 0)
+        //    {
+        //        value = 0;
+        //    }
+        //    else if (value > 100)
+        //    {
+        //        value = 100;
+        //    }
+        //    limbCount = value;
+        //}
+
+        private int limbCount = 4;
+
+        public int LimbCount
+        {
+            get { 
+                return limbCount; 
+            }
+            set {
+                if (value < 0)
+                {
+                    value = 0;
+                }
+                else if (value > 100)
+                {
+                    value = 100;
+                }
+                limbCount = value; 
+            }
+        }
+
+
 
         public string Eat(string food)
         {
-            return $"I'm a {colour} animal called {name} using some of my {limbCount} limbs to eat {food}";
+            return $"I'm a {Colour} animal called {Name} using some of my {LimbCount} limbs to eat {food}";
         }
 
         public string Move(int distance, string direction="up")
         {
-            return $"I'm a {colour} animal called {name} using some of my {limbCount} limbs to move {direction} for {distance} metres";
+            return $"I'm a {Colour} animal called {Name} using some of my {LimbCount} limbs to move {direction} for {distance} metres";
         }
     }
 }
--- OOPFun/Program.cs
+++ OOPFun/Program.cs
@@ -4,14 +4,20 @@ using OOPFun;
 Animal ani1 = new Animal();
 Animal ani2 = new Animal();
 
-ani1.name = "Janet";
-ani1.colour = "Purple";
-ani1.limbCount = 6;
+ani1.Name = "Janet";
+ani1.Colour = "Purple";
+//ani1.limbCount = -1
+//ani1.SetLimbCount(-1);
+ani1.LimbCount = -1;
 
-ani2.name = "Cindy";
-ani2.colour = "Green";
-ani2.limbCount = 8;
-ani2.health = 80;
+ani2.Name = "Cindy";
+ani2.Colour = "Green";
+ani2.LimbCount =800;
+ani2.Health = 80;
+
+//Console.WriteLine(ani2.limbCount);
+//Console.WriteLine(ani2.GetLimbCount());
+Console.WriteLine(ani2.LimbCount);
 
 Console.WriteLine(ani1.Move(10, "down"));
 Console.WriteLine(ani2.Move(15));
